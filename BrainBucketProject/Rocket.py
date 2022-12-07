c9fbd1a406e84b612061007fed540ee513a31eb7
Kseniia Andriushchenko <ksiuandriushchenko@gmail.com>
2022-12-06 22:56:27 -0600

    Rocket.py file got added

---
+++ BrainBucketProject/Rocket.py
@@ -0,0 +1,35 @@
+class Rocket:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def move_up(self):
+        print(self.y + 1)
+
+    def move_right(self):
+        print(self.x + 1)
+
+    def move_down(self):
+        print(self.y - 1)
+
+    def move_left(self):
+        print(self.x - 1)
+
+    def current_position(self):
+        print(self.x + self.y)
+
+
+move1 = Rocket(0,0)
+move1.move_up()
+
+move2 = Rocket(0,0)
+move2.move_right()
+
+move3 = Rocket(0,0)
+move3.move_down()
+
+move4 = Rocket(0,0)
+move4.move_left()
+
+current = Rocket(0,0)
+current.current_position()
+
