175dbe425bd5647548837fe9870438d995a6af6b
Kseniia Andriushchenko <ksiuandriushchenko@gmail.com>
2022-12-18 12:44:29 -0600

    Howework 17.3 Exercise #4 got added

---
+++ BrainBucketProject/InputUserProgram.py
@@ -0,0 +1,8 @@
+triangle = input("Please enter 'Start'\n")
+base = float(input("PLease enter the base\n"))
+height = float(input("Please enter the height\n"))
+area = 1/2 * base * height
+print ("Your rectangle area is ",  area)
+
+
+
