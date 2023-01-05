0c5d34758b612aa32e76524d4904bd77bd73f1da
Kseniia Andriushchenko <ksiuandriushchenko@gmail.com>
2023-01-04 23:42:40 -0600

    Homework 19.1 got added

--- BrainBucketProject/smoke_test1.py
+++ BrainBucketProject/smoke_test1.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.support.color import Color
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(executable_path="drivers/chromedriver 7")
 driver.get("https://cleveronly.com/brainbucket/index.php?route=account/login")
@@ -8,7 +11,8 @@ driver.maximize_window()
 
 logo = driver.find_element_by_xpath("//img[@title='Brainbucket']")
 
-new_registrant_btn = driver.find_element("xpath", "//a[contains(text(),'Continue')]")
+new_registrant_btn= WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(),'Continue')]")))
+#new_registrant_btn = driver.find_element("xpath", "//a[contains(text(),'Continue')]")
 background_color = new_registrant_btn.value_of_css_property("background-color")
 converted_background_color = Color.from_string(background_color)
 assert converted_background_color.rgb == 'rgb(34, 154, 200)'
