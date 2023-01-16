dcb144525f483ee4a0f6397c9730bbab39569bee
Kseniia Andriushchenko <ksiuandriushchenko@gmail.com>
2023-01-15 22:42:31 -0600

    Assignment 19.2 was added

--- BrainBucketProject/smoke_test1.py
+++ BrainBucketProject/smoke_test1.py
@@ -3,20 +3,28 @@ from selenium.webdriver.support.color import Color
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+from BrainBucketProject.webelements.browser import Browser
+from BrainBucketProject.webelements.UIElement import UIElement as Element
+from BrainBucketProject.webelements.dropdown import Dropdown
 
 driver = webdriver.Chrome(executable_path="drivers/chromedriver 7")
-driver.get("https://cleveronly.com/brainbucket/index.php?route=account/login")
+driver.get("https://cleveronly.com/brainbucket/index.php?route=common/home")
 
 driver.maximize_window()
+#clicking on 'My Account' dropdown
+account_btn = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//a[@title='My Account']")))
+account_btn.click()
 
-logo = driver.find_element_by_xpath("//img[@title='Brainbucket']")
+#WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//div[@id='top-links']/ul/li[2]/a/span[2]")))
+register_btn = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//div[@id='top-links']/ul/li[2]/ul/li/a")))
+register_btn.click()
 
-new_registrant_btn= WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(),'Continue')]")))
-#new_registrant_btn = driver.find_element("xpath", "//a[contains(text(),'Continue')]")
-background_color = new_registrant_btn.value_of_css_property("background-color")
-converted_background_color = Color.from_string(background_color)
-assert converted_background_color.rgb == 'rgb(34, 154, 200)'
-new_registrant_btn.click()
+page_title = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//div[@id= 'content']/h1")))
+assert page_title.text == 'Register Account'
+
+
 
 
 firstname_field = driver.find_element("xpath", "//fieldset/div[2]")
@@ -66,9 +74,9 @@ company_field = driver.find_element("xpath", "//input[@id='input-company']")
 company_field_class = company_field.get_attribute("class")
 assert "required" not in company_field_class
 
-company_input = driver.find_element ("id", "input-company")
+company_input = driver.find_element("id", "input-company")
 company_input.clear()
-company_input.send_keys ("AKseniia LLC")
+company_input.send_keys("AKseniia LLC")
 
 
 address_field = driver.find_element("xpath", "//fieldset[@id='address']/div[2]")
@@ -118,10 +126,46 @@ password_input.clear()
 password_input.send_keys("123456")
 password_field.click()
 
+
+
+#select country
+#country_dropdown = driver.find_element("id", "input-country")
+#country_dropdown_select = Select(country_dropdown)
+#country_dropdown_select.select_by_value("223")
+
+Dropdown(browser, By.ID, 'input-country').select_by_value('223')
+
+#select state
+#state_dropdown = driver.find_element("id", "input-zone")
+#state_dropdown_select = Select(state_dropdown)
+#state_dropdown_select.select_by_visible_text("Illinois")
+
+Dropdown(browser, By.ID, 'input-zone').select_by_text('Illinois')
+
+subscribe_btn = driver.find_element("xpath", "//div[@id='content']/form/fieldset[4]/div/div/label[2]")
+if not subscribe_btn.is_selected():
+    subscribe_btn.click()
+
+policy_btn = driver.find_element("xpath", "//div[@id='content']/form/div/div/input")
+if not policy_btn.is_selected():
+    policy_btn.click()
+
 continue_btn = driver.find_element("xpath", "//div[@id='content']/form/div/div/input[2]")
 background_color2 = continue_btn.value_of_css_property("background-color")
 converted_background_color2 = Color.from_string(background_color2)
 assert converted_background_color2.rgb == 'rgb(34, 154, 200)'
 continue_btn.click()
 
-driver.quit()
+#part2
+
+account_btn = driver.find_element("xpath", "//a[@title='My Account']")
+account_btn.click()
+
+
+login_btn = driver.find_element("xpath", "//div[@id='top-links']/ul/li[2]/ul/li[2]/a")
+login_btn.click()
+
+
+
+
+browser.shutdown()
