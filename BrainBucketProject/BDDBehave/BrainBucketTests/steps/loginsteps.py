b2b1b71d9b2349ccf62ee7d66652cde0e3db3548
Kseniia Andriushchenko <ksiuandriushchenko@gmail.com>
2023-04-20 22:19:59 -0500

    Loginsteps file got added

--- BrainBucketProject/BDDBehave/BrainBucketTests/steps/loginsteps.py
+++ BrainBucketProject/BDDBehave/BrainBucketTests/steps/loginsteps.py
@@ -4,15 +4,19 @@ from BrainBucketProject.utils.config_reader import ConfigReader
 from BrainBucketProject.webelements.browser import Browser
 from BrainBucketProject.pages.login_page import LoginPage
 from BrainBucketProject.pages.profile_page import ProfilePage
+from BrainBucketProject.webelements.UIElement import UIElement as Element
+from selenium.webdriver.common.by import By
+
 
 
 URL = "https://techskillacademy.net/brainbucket/index.php?route=account/login"
-configs = ConfigReader("BrainBucketTests/BDDBehave/logintests/steps/config.ini")
+configs = ConfigReader("BrainBucketProject/BDDBehave/BrainBucketTests/steps/config.ini")
 
-@given("user launch login page")
+@given("a web browser is at the BrainBucket login page")
 def launch_login_page(context):
     browser = Browser(URL, configs.get_browser(), configs.get_wait_time())
     context.browser = browser
+
 
 @given("user is not logged in")
 def verify_user_not_logged_in(context):
@@ -48,10 +52,49 @@ def verify_user_profile_view(context):
     When Password is entered
     And User click Login button
     Then 'Warning: No match for E-Mail Address and/or Password' will be shown
+    
 """
 
-@given("user launch login page")
-def launch_login_page(context):
-    browser = Browser(URL, configs.get_browser(), configs.get_wait_time())
-    context.browser = browser
+
+
+@when("password is entered")
+def enter_password(context):
+    login_page = context.login_page
+    login_page.enter_password(configs.get_user1_password())
+
+@when("user click Login button")
+def click_login_button(context):
+    login_page = context.login_page
+    login_page.click_login_button()
+
+@then("'Warning: No match for E-Mail Address and/or Password' will be shown")
+def verify_warning(context):
+    warning_title = Element(context.browser, By.XPATH, "//div[contains(text(), 'Warning: No match')]")
+    assert warning_title.get_text() == 'Warning: No match for E-Mail Address and/or Password.'
+
+"""Scenario: user can recover his password
+     Given User is not logged in
+     When User clicks 'Forgotten Password' button
+     And enters his email
+     Then Message 'An email with a confirmation link has been sent your email address.' will be dispalyed
+"""
+
+
+@when("user clicks 'Forgotten Password' button")
+def click_forgot_pass_btn(context):
+    login_page = context.login_page
+    login_page.click_forgotten_pass_btn()
+
+@when("enters his email")
+def enter_email(context):
+    login_page = context.login_page
+    login_page.enter_email(configs.get_user1_email())
+
+@then("Message 'An email with a confirmation link has been sent your email address.' will be dispalyed")
+def verify_email_sent(context):
+    message_text = Element(context.browser, By.XPATH, "//div[contains(text(), 'An email with a confirmation')]")
+    assert message_text.get_text() == 'An email with a confirmation link has been sent your email address.'
+
+
+
 
