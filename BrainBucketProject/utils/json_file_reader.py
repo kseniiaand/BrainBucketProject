c366df6eed760e52603c94fc6f5ae360e8d94a83
Kseniia Andriushchenko <ksiuandriushchenko@gmail.com>
2023-05-17 22:46:08 -0500

    json_file_reader.py got added

--- BrainBucketProject/utils/json_file_reader.py
+++ BrainBucketProject/utils/json_file_reader.py
@@ -1,5 +1,6 @@
 import json
 
+
 class JsonFileReader:
     def __init__(self, filename):
         self.data = None
@@ -37,4 +38,27 @@ class JsonFileReader:
             raise Exception("Length option is not present in the config file")
         return self.data['length']
 
+    def get_user1_email(self):
+        if 'email' not in self.data.keys():
+            raise Exception("email option is not found in user1 section")
+        return self.data['email']
 
+    def get_user1_password(self):
+        if 'password' not in self.data.keys():
+            raise Exception("password option is not found in iser1 section")
+        return self.data['password']
+
+    def get_url(self):
+        if 'url' not in self.data.keys():
+            raise Exception("URL option is not present in the config file")
+        return self.data['url']
+
+    def get_username(self):
+        if 'username' not in self.data.keys():
+            raise Exception("username option is not found in BrowserStack section")
+        return self.data['username']
+
+    def get_accesskey(self):
+        if 'accesskey' not in self.data.keys():
+            raise Exception("Access key option is not found in BrowserStack section")
+        return self.data['accesskey']
